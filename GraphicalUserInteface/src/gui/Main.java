e78fc5870f2a959e124052eb7c20d46e9bc541e1
Nadir Elmakias <nadir070299e@gmail.com>
2024-09-05 22:18:38 +0300

    Added ui SubComponents fxmls and css files

--- GraphicalUserInteface/src/gui/Main.java
+++ GraphicalUserInteface/src/gui/Main.java
@@ -1,25 +1,27 @@
 package gui;
 
+import gui.grid.GridBuilder;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-
-        Parent root = FXMLLoader.load(Main.class.getResource("resources/MainAppView.fxml"));
+        GridBuilder gridBuilder = new GridBuilder(50, 20, 35, 110);
+        BorderPane root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("resources/MainAppView.fxml")));
+        root.setCenter(gridBuilder.build());
         Scene scene = new Scene(root, 800, 600);
-        stage.setTitle("Hello World");
+        stage.setTitle("Hello SASON");
+        stage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("resources/naruto-wallpaper-2560x1440.jpg"))));
         stage.setScene(scene);
         stage.show();
     }
-    
-  
     
     public static void main(String[] args) {
         launch(args);
---
+++ GraphicalUserInteface/src/gui/grid/GridBuilder.java
@@ -0,0 +1,140 @@
+package gui.grid;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.*;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class GridBuilder {
+    
+    private final int numOfRows;
+    private final int numOfCols;
+    private final int rowHeight;
+    private final int columnWidth;
+    
+    public GridBuilder(int row, int col, int rowHeight, int colWidth) {
+        this.numOfRows = row;
+        this.numOfCols = col;
+        this.rowHeight = rowHeight;
+        this.columnWidth = colWidth;
+    }
+    
+    public ScrollPane build() throws IOException {
+        
+        ScrollPane root = new ScrollPane();
+        GridPane grid = new GridPane();
+        
+        this.buildScrollPane(root);
+        this.buildGridPane(grid);
+        this.buildRowConstraints(grid);
+        this.buildColumnConstraints(grid);
+        
+        ObservableList<Node> children = grid.getChildren();
+        
+        this.buildHeadersRow(children);
+        this.buildHeadersColumn(children);
+        this.buildCellsComponents(children);
+        
+        root.setContent(grid);
+        
+        return root;
+    }
+    
+    private void buildCellsComponents(ObservableList<Node> children) throws IOException {
+        for (int i = 1; i <= this.numOfRows; i++) {
+            for (int j = 1; j <= this.numOfCols; j++) {
+                Parent cell = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/gui/resources/CellSubComponent.fxml")));
+                GridPane.setColumnIndex(cell, j);
+                GridPane.setRowIndex(cell, i);
+                children.add(cell);
+            }
+        }
+    }
+    
+    private void buildHeadersColumn(ObservableList<Node> children) {
+        for (int i = 1; i <= this.numOfRows; i++) {
+            Button button = new Button(i < 10 ? "0" + i : i + "");
+            button.setMaxHeight(Double.MAX_VALUE);
+            button.setMaxWidth(Double.MAX_VALUE);
+            GridPane.setRowIndex(button, i);
+            children.add(button);
+        }
+    }
+    
+    private void buildHeadersRow(ObservableList<Node> children) {
+        for (int i = 0; i < this.numOfCols; i++) {
+            Button button = new Button(Character.toString((char) i + 'A'));
+            button.setMaxHeight(Double.MAX_VALUE);
+            button.setMaxWidth(Double.MAX_VALUE);
+            GridPane.setColumnIndex(button, i + 1);
+            children.add(button);
+        }
+    }
+    
+    private void buildGridPane(GridPane grid) {
+        grid.setAlignment(Pos.CENTER); // Set alignment to center
+        grid.setMaxHeight(Double.MAX_VALUE); // Set to use maximum available height
+        grid.setMaxWidth(Double.MAX_VALUE);  // Set to use maximum available width
+        grid.getStyleClass().add("sheet-grid-pane");
+    }
+    
+    private void buildScrollPane(ScrollPane scrollPane) {
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setMaxHeight(Double.MAX_VALUE);
+        scrollPane.setMaxWidth(Double.MAX_VALUE);
+        scrollPane.setMinHeight(0);
+        scrollPane.setMinWidth(0);
+        scrollPane.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/gui/resources/MainGridComponent.css")).toExternalForm());
+    }
+    
+    private void buildRowConstraints(GridPane grid) {
+        ObservableList<RowConstraints> rowConstraints = grid.getRowConstraints();
+        
+        for (int i = 0; i <= this.numOfRows; i++) {
+            RowConstraints currentRow = new RowConstraints();
+            
+            currentRow.setVgrow(Priority.ALWAYS);
+            if (i == 0) {
+                currentRow.setMaxHeight(35);
+                currentRow.setMinHeight(35);
+                currentRow.setPrefHeight(35);
+            } else {
+                currentRow.setMaxHeight(Double.MAX_VALUE);
+                currentRow.setMinHeight(this.rowHeight);
+                currentRow.setPrefHeight(this.rowHeight);
+            }
+            
+            rowConstraints.add(currentRow);
+        }
+    }
+    
+    private void buildColumnConstraints(GridPane grid) {
+        ObservableList<ColumnConstraints> columnConstraints = grid.getColumnConstraints();
+        
+        for (int i = 0; i <= this.numOfCols; i++) {
+            ColumnConstraints currentColumn = new ColumnConstraints();
+            
+            currentColumn.setHgrow(Priority.ALWAYS);
+            if (i == 0) {
+                currentColumn.setMaxWidth(35);
+                currentColumn.setMinWidth(35);
+                currentColumn.setPrefWidth(35);
+            } else {
+                currentColumn.setMaxWidth(Double.MAX_VALUE);
+                currentColumn.setMinWidth(this.columnWidth);
+                currentColumn.setPrefWidth(this.columnWidth);
+            }
+            
+            columnConstraints.add(currentColumn);
+        }
+    }
+}
